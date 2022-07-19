bf188b31e0f4cfbdde559de9138a44209ec410d7
htustun <htustun@gmail.com>
2022-07-19 19:50:25 +0300

    codewars4

---
+++ codeWars/codewars4/sumTwoSmallestNumbers.js
@@ -0,0 +1,11 @@
+function sumTwoSmallestNumbers(numbers) {
+    const numbersInt =[];
+    for (let index = 0; index < numbers.length; index++) {
+        if (numbers[index] > 0 && Number.isInteger(numbers[index])) {
+            numbersInt.push(numbers[index]);
+        }
+    }
+    numbersInt.sort((a, b) => a - b);
+    return numbersInt[0]+numbersInt[1];
+  }
+  console.log(sumTwoSmallestNumbers([3, -1, 0.5, 19, 22]));
